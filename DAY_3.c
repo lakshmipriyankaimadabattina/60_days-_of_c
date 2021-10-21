ddebc1f39afb4309c2c19d855ddae2bf6c821ace
Lakshmi priyanka <89516421+lakshmipriyankaimadabattina@users.noreply.github.com>
2021-10-21 16:05:00 +0530

    Add files via upload

---
+++ DAY_3.c
@@ -0,0 +1,24 @@
+
+/*
+Program to accept values of two numbers and print their addition.*/
+#include<stdio.h>
+
+int main()
+{
+  int a,b,ans;
+  
+
+  printf("Enter 1st number:");
+  scanf("%d",&a);
+
+  printf("Enter 2nd number:");
+  scanf("%d",&b);
+
+  ans = a + b;
+
+  printf("Addition is : %d",ans);
+
+  return 0;
+}
+/*#basics 
+DAY_3*/
